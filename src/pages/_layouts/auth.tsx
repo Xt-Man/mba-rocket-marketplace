8923cd036a08adf0d3bbb5ac2f3497adc29089ca
Edgar Bottini <edgarbottini@hotmail.com>
2024-10-08 22:12:37 -0300

    wip: sign-in layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -2,11 +2,15 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
-
-      <div>
-        <Outlet />
+    <div className="bg-shape-bg container m-auto flex min-h-[768px]">
+      <div className="w-[755px] pt-8">
+        <img className="ml-8" src="./logo.png" alt="" />
+        <img src="./sign.png" alt="" />
+      </div>
+      <div className="w-[611px] p-8">
+        <div className="h-full rounded-[32px] bg-white px-20 py-16">
+          <Outlet />
+        </div>
       </div>
     </div>
   )
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,70 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInForm = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+  const [searchParams] = useSearchParams()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    defaultValues: {
+      email: searchParams.get('email') ?? '',
+      password: '',
+    },
+  })
+
+  async function handleSignIn(data: SignInForm) {
+    console.log(data)
+  }
+
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div>
+        <h1>Acesse sua conta</h1>
+        <h3>Informe seu e-mail e senha para entrar</h3>
+      </div>
+
+      <div className="">
+        <div className="">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className="space-y-2">
+              <Label htmlFor="email">E-MAIL</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">SENHA</Label>
+              <Input id="password" type="password" {...register('password')} />
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar
+            </Button>
+          </form>
+        </div>
+        <div className="">
+          <Label>Ainda não tem uma conta?</Label>
+          <Button disabled={isSubmitting} className="w-full" type="submit">
+            Cadastrar
+          </Button>
+        </div>
+      </div>
     </>
   )
 }
